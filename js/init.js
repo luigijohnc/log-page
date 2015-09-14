7108eb2afa2bde2cdecc1f5da4e0153305db4b6e
zeptosphyr <zephyr.404.not.found@gmail.com>
2015-09-14 09:55:30 +0800

    Added Date filter

--- js/init.js
+++ js/init.js
@@ -1,7 +1,12 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $('ul.tabs').tabs();
+        $('ul.tabs').tabs();
+        
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+        });
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
